b3c4f6c5ec3e10ba648d880ebfca82689fa0621d
Alena Yashchanka <lena1509y@yandex.ru>
2019-01-07 22:29:05 +0200

    add !!!

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
 const part1 = 'Hello';
 const part2 = 'World';
 function message (str1, str2) {
-    console.log(`${str1} ${str2}`);
+    console.log(`${str1} ${str2}!!!`);
 }
 message(part1, part2);
