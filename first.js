35c921e467fc31c0fa9d253422e7fe9d542add6b
Alena Yashchanka <lena1509y@yandex.ru>
2019-01-07 22:26:43 +0200

    third commit

--- first.js
+++ first.js
@@ -1,5 +1,6 @@
-const phrase = 'Hello';
-function message (str) {
-    console.log(str);
+const part1 = 'Hello';
+const part2 = 'World';
+function message (str1, str2) {
+    console.log(`${str1} ${str2}`);
 }
-message(phrase);
+message(part1, part2);
